07dec71d3ff906b6b80e8fe44922d9f35dfca789
douglas.boeno <douglasrodriguesboeno@gmail.com>
2022-10-18 15:27:25 -0300

    first commit logo

--- frontend/src/Components/Template/Logo.jsx
+++ frontend/src/Components/Template/Logo.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Logo.css'
+import logo from '../../assets/imgs/Logo.png'
 
 export default props => {
     return <aside className='logo'>
-        Logo
+        <a href='/' className='logo'>
+            <img src={logo} alt="logo"></img>
+        </a>
     </aside>
 }
