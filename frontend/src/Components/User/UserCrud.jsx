bb2f6bd80be8bf5fc51ab11508ba3995e7997033
douglas.boeno <douglasrodriguesboeno@gmail.com>
2022-10-19 15:01:51 -0300

    inital crud

---
+++ frontend/src/Components/User/UserCrud.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import Main from "../Template/Main"
+
+const headerProps = {
+    icon: 'users',
+    titulo: 'Usuarios',
+    subtitulo: 'Cadastro usuários'
+}
+
+export default class Crud extends Component {
+    render() {
+        return (
+            <Main {...headerProps}>
+                Cadastro de usuários 
+            </Main>
+        )
+    }
+}
