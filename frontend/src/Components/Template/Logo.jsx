35f9c1a3c8341592a8e55d04f1a3880585f2f2f6
douglas.boeno <douglasrodriguesboeno@gmail.com>
2022-10-22 18:11:38 -0300

    alteracoes responsividade

--- frontend/src/Components/Template/Logo.jsx
+++ frontend/src/Components/Template/Logo.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Logo.css'
-import logo from '../../assets/imgs/Logo.png'
+//import logo from '../../assets/imgs/Logo.png'
 import { Link } from 'react-router-dom'
 
 export default props => {
     return <aside className='logo'>
         <Link to='/' className='logo'>
-            <img src={logo} alt="logo"></img>
+            <img alt="logo"></img>
         </Link>
     </aside>
 }
